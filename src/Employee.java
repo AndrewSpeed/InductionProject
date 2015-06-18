bf62d1075d6de1649c5c67e307fe8968372fdad3
Andrew Speed <A.Speed@kainos.com>
2015-06-18 16:45:36 +0100

    Added ability to create employees, altered queries to retrieve employee details.

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,6 @@
 import java.math.BigDecimal;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Employee {
 	
@@ -8,47 +9,88 @@ public class Employee {
 	private String lastname;		//Employee Surname
 	private BigDecimal salary;			//Employee Salary
 	
-	public Employee(int newId){
-		setId(newId);
-	}
 	
-	public Employee(int newId, String eForename, String eLastname, BigDecimal eSalary) {
-		this(newId);
+	public Employee(String eForename, String eLastname, BigDecimal eSalary) {
 		this.forename = eForename;
 		this.lastname = eLastname;
 		this.salary = eSalary;
 	}
 	
-	private int getId() {
+	public Employee(int newId, String eForename, String eLastname, BigDecimal eSalary) {
+		this(eForename, eLastname, eSalary);
+		this.id = newId;
+	}
+	
+	public int getId() {
 		return id;
 	}
 	
-	private void setId(int id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	
-	private String getForename() {
+	public String getForename() {
 		return forename;
 	}
 	
-	private void setForename(String forename) {
+	public void setForename(String forename) {
 		this.forename = forename;
 	}
 	
-	private String getLastname() {
+	public String getLastname() {
 		return lastname;
 	}
 	
-	private void setLastname(String lastname) {
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
 	
-	private BigDecimal getSalary() {
+	public BigDecimal getSalary() {
 		return salary;
 	}
 	
-	private void setSalary(BigDecimal salary) {
+	public void setSalary(BigDecimal salary) {
 		this.salary = salary;
+	}
+	
+	public static int validateUserID(Scanner input){
+		int newInt = -1;
+		try{
+			newInt = input.nextInt();
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Wrong input. Closing system.");
+			System.exit(1);
+		}
+		return newInt;
+		
+		/*while(!input.hasNextInt()){
+            System.out.println("That's not a number!");
+            return validateUserID(input);
+        }//while
+		return input.nextInt();*/
+	}
+	
+	public static String validateStringInput(String name){
+		while ((name.length() > 30) || (name.length() < 1) || (name.matches(".*\\d.*"))) {
+			System.out.println("Name is not valid. Please re-enter: ");
+			name = Main.input.next();
+			//Main.input.next();
+		}//while
+		
+		return name;	
+	}
+	
+	public static BigDecimal validateSalaryInput(Scanner input) {
+		BigDecimal sal = null;
+		try {
+			sal = input.nextBigDecimal();
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input. Exiting.");
+			System.exit(1);
+		}
+		return sal;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,163 +1,198 @@
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	static Connection conn;
 	static Statement stmt;
 	static String url = "jdbc:mysql://localhost:3306/project_employees";
-	
-	static Scanner input = new Scanner(System.in);
-	
-	
-	public static void main(String[] args){	
-		
-		 //Scanner input = new Scanner(System.in);
-		
-		//load the JDBC driver		
-		loadJDBCdriver();
-		
-		//setting up connection
-		setUpConnection();
-		
-		
-		System.out.println("\nWELCOME TO THE EMPLOYEE MANAGEMENT SYSTEM\n*** Main Menu ***\n");
-		System.out.println("1. List all employees\n2. Search for employee by ID\n3. Add new employee\n\nPlease type your option number: ");
-		
-		int option = input.nextInt();
-		
-		switch (option){
-		
-		case 1:
-			//select all employees
-			System.out.println("all employees will be listed here");
-			break;
-		case 2:
-			//search by ID
-			System.out.println("employee with ");
-			break;
-		case 3:
-			//add employee
-			addEmployee();
-			
-			
-			
-			
-		default:
-			System.out.println("Sorry, your input was not recognised.");
-			break;
-		
-		}
-		
-		
-		
-		try{
-		
-		stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
-		
-		//ArrayList results = new ArrayList(rs);
-		while(rs.next()){
-			
-            int customerID = rs.getInt("id");
-            String fname = rs.getString("forename");
-            String lname = rs.getString("surname");
-            int salaryID = rs.getInt("salaryid");
-            System.out.println(customerID + "\t" + fname +
-                               "\t" + lname + "\t" + salaryID);	
-		}
-		}
-		catch(SQLException e){
-			System.err.print(e.getMessage());			
-		}
-		
-		
-		
-		}//psvm
-	
-	
-	//METHODS
 
-	public static void addEmployee(){
-		System.out.println("Please enter a forename up to 30 characters long: ");
-		String fnametemp = input.next();
-		
-		while((fnametemp.length() > 30) || (fnametemp.length() < 1)){
-			System.out.println("Name is not a valid length. Please re-enter: ");
-			fnametemp = input.nextLine();
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		// Scanner input = new Scanner(System.in);
+
+		// load the JDBC driver
+		loadJDBCdriver();
+
+		// setting up connection
+		setUpConnection();
+
+		System.out
+				.println("\nWELCOME TO THE EMPLOYEE MANAGEMENT SYSTEM\n*** Main Menu ***\n");
+
+		while (true) {
+			int option = getChoice();
+			switch (option) {
+
+			case 1:
+				// select all employees
+				selectAllEmployees();
+
+				break;
+			case 2:
+				// search by ID
+
+				try {
+					stmt = conn.createStatement();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.out.println("Please enter the user ID: ");
+				int userID = input.nextInt();
+
+				try {
+					ResultSet rs = stmt
+							.executeQuery(selectEmployeeByID(userID));
+					while (rs.next()) {
+						int empID = rs.getInt("ID");
+						String fname = rs.getString("forename");
+						String sname = rs.getString("surname");
+						int salary = rs.getInt("salary");
+						System.out.println("Emp ID\tForename Surname Salary\n");
+						System.out.println(empID + "\t" + fname + "\t" + sname
+								+ "\t" + salary + "\n");
+					}
+
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				break;
+			case 3:
+				// add employee
+				addEmployee();
+				break;
+
+			default:
+				System.out.println("Invalid option selection, exiting.");
+				System.exit(1);
+				break;
+			}
 		}
-		
-		System.out.println("Please enter a surname up to 30 characters long: ");
-		String snametemp = input.next();
-		while((snametemp.length() > 30) || (snametemp.length() < 1)){
-			System.out.println("Name is not a valid length. Please re-enter: ");			
-			snametemp = input.nextLine();
+
+	}// psvm
+
+	// METHODS
+
+	public static int getChoice() {
+		System.out
+				.println("1. List all employees\n2. Search for employee by ID\n3. Add new employee\n\nPlease type your option number: ");
+		int option = 0;
+		try {
+			option = input.nextInt();
+		} catch (InputMismatchException e) {
+			System.err.print(e.getMessage());
+			System.exit(1);
 		}
-		
-		System.out.println("Please choose a salary ID from the following list : \n1\n2\n3\n ");
-		int salaryIDtemp = input.nextInt();
-		if(salaryIDtemp <1 || salaryIDtemp > 3){
-			System.out.println("Salary is not from the list shown. Please re-enter: ");
-			salaryIDtemp = input.nextInt();
-		}
+		return option;
 	}
-	
-	
-	
+
+	public static void addEmployee() {
+		System.out
+				.println("Please enter a forename up to 30 characters long: ");
+		String inputFName = input.next();
+		Employee.validateStringInput(inputFName);
+
+		System.out.println("Please enter a surname up to 30 characters long: ");
+		String inputSName = input.next();
+		Employee.validateStringInput(inputSName);
+
+		System.out.println("Please enter a salary: ");
+		BigDecimal inputSalary = Employee.validateSalaryInput(input);
+
+		createEmployeeInDB(inputFName, inputSName, inputSalary);
+
+	}
+
+	public static void createEmployeeInDB(String forename, String surname,
+			BigDecimal salary) {
+		String sqlTemplate = "BEGIN; "
+				+ "INSERT INTO salaries(salary) VALUES (?); "
+				+ "INSERT INTO employees(forename, surname, salaryid) VALUES (?, ?, LAST_INSERT_ID()); "
+				+ "COMMIT;";
+
+		PreparedStatement sql;
+		try {
+			sql = conn.prepareStatement(sqlTemplate);
+			sql.setBigDecimal(1, salary);
+			sql.setString(2, forename);
+			sql.setString(3, surname);
+			System.out.println(sql);
+			sql.executeQuery();
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		System.out.println("User added successfully :).");
+	}
+
 	/*
 	 * selectAllEmployees method to return all Employees
-	 */	
-	public static void selectAllEmployees(){
-		//String queryString = ("Select * from employees");
-		//return queryString;
-		try{
-		stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
-		}
-		catch(SQLException e){
+	 */
+	public static void selectAllEmployees() {
+		// String queryString = ("Select * from employees");
+		// return queryString;
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt
+					.executeQuery("SELECT id, forename, surname, salary "
+							+ "FROM employees "
+							+ "INNER JOIN salaries ON employees.salaryid=salaries.salaryid;");
+			EmployeeCollection<Employee> employees = new EmployeeCollection(rs);
+			for (Employee e : employees) {
+				System.out.println(String.format("%d %s %s %.2f", e.getId(),
+						e.getForename(), e.getLastname(), e.getSalary()));
+			}
+		} catch (SQLException e) {
 			e.getMessage();
 		}
 	}
-	
+
 	/*
 	 * selectQuery method to run select query using different userID's
 	 */
-	public static String selectEmployeeByID(int userID){
-		String queryString = ("Select * from employees WHERE id =" + userID);
+	public static String selectEmployeeByID(int userID) {
+		String queryString = ("SELECT id, forename, surname, salary " + 
+							  "FROM employees INNER JOIN salaries ON employees.salaryid=salaries.salaryid " +
+							  "WHERE id=" + userID + ";");
 		return queryString;
 	}
-	
-	
+
 	/*
 	 * loadJDBCdriver method loads the JDBC driver
 	 */
-	public static void loadJDBCdriver(){
-		//load the JDBC driver
-				try{
-					Class.forName("com.mysql.jdbc.Driver");
-					
-				}catch(ClassNotFoundException e){
-					System.err.print(e.getMessage());
-				}	
+	public static void loadJDBCdriver() {
+		// load the JDBC driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+		} catch (ClassNotFoundException e) {
+			System.err.print(e.getMessage());
+		}
 	}
-	
+
 	/*
 	 * setUpConnection method sets up the connection to the database via a URL
 	 */
-	public static void setUpConnection(){
-		//setting up connection
-		try{
-					
-			conn = DriverManager.getConnection(url, "root", "password");		
-				
-			}
-		catch(SQLException e){
-					System.err.print(e.getMessage());
-			}
+	public static void setUpConnection() {
+		// setting up connection
+		try {
+
+			conn = DriverManager.getConnection(url, "root", "Kainos");
+
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+		}
 	}
-	
-}//class
+
+}// class

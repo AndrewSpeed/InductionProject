711fe361ebbaacd69b749fe5bce86de878614129
Lizzy Atkinson <l.atkinson@kainos.com>
2015-06-18 13:17:54 +0100

    Switch construct altered with new methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -10,15 +11,48 @@ public class Main {
 	static Statement stmt;
 	static String url = "jdbc:mysql://localhost:3306/project_employees";
 	
+	static Scanner input = new Scanner(System.in);
+	
 	
 	public static void main(String[] args){	
 		
+		 //Scanner input = new Scanner(System.in);
 		
 		//load the JDBC driver		
 		loadJDBCdriver();
 		
 		//setting up connection
 		setUpConnection();
+		
+		
+		System.out.println("\nWELCOME TO THE EMPLOYEE MANAGEMENT SYSTEM\n*** Main Menu ***\n");
+		System.out.println("1. List all employees\n2. Search for employee by ID\n3. Add new employee\n\nPlease type your option number: ");
+		
+		int option = input.nextInt();
+		
+		switch (option){
+		
+		case 1:
+			//select all employees
+			System.out.println("all employees will be listed here");
+			break;
+		case 2:
+			//search by ID
+			System.out.println("employee with ");
+			break;
+		case 3:
+			//add employee
+			addEmployee();
+			
+			
+			
+			
+		default:
+			System.out.println("Sorry, your input was not recognised.");
+			break;
+		
+		}
+		
 		
 		
 		try{
@@ -46,13 +80,47 @@ public class Main {
 		}//psvm
 	
 	
+	//METHODS
+
+	public static void addEmployee(){
+		System.out.println("Please enter a forename up to 30 characters long: ");
+		String fnametemp = input.next();
+		
+		while((fnametemp.length() > 30) || (fnametemp.length() < 1)){
+			System.out.println("Name is not a valid length. Please re-enter: ");
+			fnametemp = input.nextLine();
+		}
+		
+		System.out.println("Please enter a surname up to 30 characters long: ");
+		String snametemp = input.next();
+		while((snametemp.length() > 30) || (snametemp.length() < 1)){
+			System.out.println("Name is not a valid length. Please re-enter: ");			
+			snametemp = input.nextLine();
+		}
+		
+		System.out.println("Please choose a salary ID from the following list : \n1\n2\n3\n ");
+		int salaryIDtemp = input.nextInt();
+		if(salaryIDtemp <1 || salaryIDtemp > 3){
+			System.out.println("Salary is not from the list shown. Please re-enter: ");
+			salaryIDtemp = input.nextInt();
+		}
+	}
+	
+	
 	
 	/*
 	 * selectAllEmployees method to return all Employees
-	 */
-	public static String selectAllEmployees(){
-		String queryString = ("Select * from employees");
-		return queryString;
+	 */	
+	public static void selectAllEmployees(){
+		//String queryString = ("Select * from employees");
+		//return queryString;
+		try{
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
+		}
+		catch(SQLException e){
+			e.getMessage();
+		}
 	}
 	
 	/*

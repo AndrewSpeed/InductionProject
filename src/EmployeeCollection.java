66bdc3e72666ab1a9359a40dd9ac273e82429ce6
Andrew Speed <A.Speed@kainos.com>
2015-06-18 12:31:29 +0100

    Added collection for employees, Employee class still in progress

---
+++ src/EmployeeCollection.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class EmployeeCollection<E> extends ArrayList<E> {
+	
+	/**
+	 * default UID for EmployeeCollection
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeCollection(ResultSet results) {
+		super();
+		try {
+			while(results.next()) {
+				int empID = results.getInt(1);
+				String empForename = results.getString(2);
+				String empSurname = results.getString(3);
+				BigDecimal empSalary = results.getBigDecimal(4);
+				
+				Employee emp = new Employee(empID, empForename, empSurname, empSalary);
+				this.add(emp);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+	}
+
+}
